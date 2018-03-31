cf4ecceb5c68e7e250a87ec72e0de2af4bb21493
Marat Valiev <mz.valiev@gmail.com>
2018-03-30 22:22:07 -0400

    js file refactoring

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const api = "https://fcc-weather-api.glitch.me/api/current?";
+const api = 'https://fcc-weather-api.glitch.me/api/current?';
 
-$(document).ready(function() {
+$(function() {
   //Get latitude and longitude using ip address
   $.getJSON('https://geoip.nekudo.com/api/en/', function(json) {
     const lat = json.location.latitude;
@@ -9,17 +9,17 @@ $(document).ready(function() {
 
     //Get location and weather conditions using latitude and longitude
     $.getJSON(url, function(get) {
-        const city = get.name,
-        country = get.sys.country,
-        tempC = get.main.temp.toFixed(0),
-        weather = get.weather[0].main,
-        icon = get.weather[0].icon;
-      $("#location").html(`${city}, ${country}`);
-      $("#weather").html(`${weather}</br><img src="${icon}" alt="Weather icon"/>`);
+        const city = get.name;
+        const country = get.sys.country;
+        const tempC = get.main.temp.toFixed(0);
+        const  weather = get.weather[0].main;
+        const icon = get.weather[0].icon;
+      $('#location').html(`${city}, ${country}`);
+      $('#weather').html(`${weather}</br><img src='${icon}' alt='Weather icon'/>`);
 
       //Toggle button to display temperature in selected unit
       const tempF = Math.round(parseInt(tempC) * 9 / 5 + 32);
-      $("#temperature").text(`${tempC} C°`);
+      $('#temperature').text(`${tempC} C°`);
       $(function() {
         $('#unit-converter').change(function() {
           if ($(this).prop('checked')) {
@@ -27,33 +27,33 @@ $(document).ready(function() {
           } else {
             $('#temperature').html(`${tempC} °C`);
           }
-        })
-      })
+        });
+      });
 
       //Change background image depending on weather
-      const cloudyImage = "images/clouds.jpg",
-        clearImage = "images/clear.jpg",
-        snowImage = "images/snow.jpg",
-        rainImage = "images/rain1.png",
-        nightClearImage = "images/night_clear.jpg",
-        fogImage = "images/fog.jpg",
-        mistImage = "images/mist.jpg";
+      const cloudyImage = 'images/clouds.jpg';
+      const clearImage = 'images/clear.jpg';
+      const snowImage = 'images/snow.jpg';
+      const rainImage = 'images/rain1.png';
+      const nightClearImage = 'images/night_clear.jpg';
+      const fogImage = 'images/fog.jpg';
+      const mistImage = 'images/mist.jpg';
 
-      if (weather == "Clear") {
-        $("body").css("background", "url(" + clearImage + ")");
-      } else if (weather == "Clouds") {
-        $("body").css("background", "url(" + cloudyImage + ")");
-      } else if (weather == "Snow") {
-        $("body").css("background", "url(" + snowImage + ")");
-      } else if (weather == "Rain") {
-        $("body").css("background", "url(" + rainImage + ")");
-      } else if (weather == "Night-Clear") {
-        $("body").css("background", "url(" + nightClearImage + ")");
-      } else if (weather == "Fog") {
-        $("body").css("background", "url(" + fogImage + ")");
-      } else if (weather == "Mist") {
-        $("body").css("background", "url(" + mistImage + ")");
+      if (weather == 'Clear') {
+        $('body').css('background', 'url(' + clearImage + ')');
+      } else if (weather == 'Clouds') {
+        $('body').css('background', 'url(' + cloudyImage + ')');
+      } else if (weather == 'Snow') {
+        $('body').css('background', 'url(' + snowImage + ')');
+      } else if (weather == 'Rain') {
+        $('body').css('background', 'url(' + rainImage + ')');
+      } else if (weather == 'Night-Clear') {
+        $('body').css('background', 'url(' + nightClearImage + ')');
+      } else if (weather == 'Fog') {
+        $('body').css('background', 'url(' + fogImage + ')');
+      } else if (weather == 'Mist') {
+        $('body').css('background', 'url(' + mistImage + ')');
       }
-    })
+    });
   });
-})
+});
